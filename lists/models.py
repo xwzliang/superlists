5da4a4a6054d84eed45fb100b105fedd33353665
broliang <18706703437@163.com>
2019-05-13 20:48:44 +0800

    Add a foreign key relationship between List and Item in the models to give Item a list attribute

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-class Item(models.Model):
-    text = models.TextField(default='')
-
 class List(models.Model):
 
     def save(self):
         pass
+
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
+
