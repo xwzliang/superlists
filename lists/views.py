676e7cb59d5ca30d6987e816eb3c6ed4d75b1fe3
broliang <18706703437@163.com>
2019-05-14 20:48:02 +0800

    Capture parameters from URLs

--- lists/views.py
+++ lists/views.py
@@ -4,8 +4,9 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
